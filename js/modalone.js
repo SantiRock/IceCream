cfa28ba0379360141300845f5345bcff249cdb8d
Santiago Quintero <santiago.qp86@gmail.com>
2022-12-01 20:03:21 -0500

    animation

--- js/modalone.js
+++ js/modalone.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector("[data-open]"),
       closeModalBtn: document.querySelector("[data-close]"),
-      modal: document.querySelector("[data]"),
+      modal: document.querySelector("[data-modal1]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
